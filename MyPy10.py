cfed757f3a0ef6583c8d7b45faeb64e831d4337d
hannmx <mxkud@mail.ru>
2023-02-04 13:43:50 +0300

    Сделанная программа вывода списка и подсчета суммы нечетных элементов списка

---
+++ MyPy10.py
@@ -0,0 +1,12 @@
+import random
+list_rand = []
+summ = 0
+for i in range(random.randint(3,10)):
+    list_rand.append(random.randint(0,10))
+print(f'Новый список: {list_rand}')
+
+for i in range(len(list_rand)):
+    if i % 2 != 0:
+        summ += list_rand[i]
+
+print(f'Сумма нечетных элементов списка: {summ}')
